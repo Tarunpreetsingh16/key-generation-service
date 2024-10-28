558f75fb47b3de1c38716744969227e1b7873f71
Jimmy <tarunpreetsingh16@gmail.com>
2024-10-27 22:52:25 -0400

    added swagger

--- src/main/java/com/jim_jam/key_generation_service/common/error/ErrorDetail.java
+++ src/main/java/com/jim_jam/key_generation_service/common/error/ErrorDetail.java
@@ -1,5 +1,6 @@
 package com.jim_jam.key_generation_service.common.error;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,8 +13,14 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Schema(name = "ErrorDetail", description = "Class to store details of an error")
 public class ErrorDetail {
+    @Schema(name = "title", description = "Title of an error", example = "Something went wrong.")
     private String title;
+
+    @Schema(name = "titleKey", description = "Key to identify an error", example = "something.went.wrong")
     private String titleKey;
+
+    @Schema(name = "detail", description = "Explanation of an error", example = "Something happened when running the API.")
     private String detail;
 }
--- src/main/java/com/jim_jam/key_generation_service/models/GetKeyResponse.java
+++ src/main/java/com/jim_jam/key_generation_service/models/GetKeyResponse.java
@@ -1,5 +1,6 @@
 package com.jim_jam.key_generation_service.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.Builder;
 
@@ -8,6 +9,8 @@ import lombok.Builder;
  */
 @Builder
 @Data
+@Schema(name = "GetKeyResponse", description = "Model to store data to be returned when a new key needs to be generated")
 public class GetKeyResponse {
+    @Schema(name = "key", description = "Random key generated", example = "ae34fc")
     private String key;
 }
